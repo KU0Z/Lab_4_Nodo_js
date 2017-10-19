dbc1fb99c3d7bbf7af69c689e20dbce8fa50c125
KU0Z <koubedo@correo.url.edu.gt>
2017-10-18 20:59:58 -0600

    terminado mas estilo

--- Lab_4/Lab_4_Javascript.js
+++ Lab_4/Lab_4_Javascript.js
@@ -5,8 +5,49 @@
            var count = 0;
            var _key="";
            var _value="";
+           var flag = false;
+           if(dictionary.length == 0)
+           {
             _key = document.getElementById("txt1").value;
-            _value = document.getElementById("txt2").value;
+            flag = true;
+           }
+           else{
+            for (var i = 0; i <dictionary.length; i++) {
+                if(dictionary[i].key === document.getElementById("txt1").value)
+                {
+                    flag = false;
+                    i = dictionary.length;
+                } 
+                else{
+                    flag = true;
+                    _key = document.getElementById("txt1").value;
+                }
+           }    
+        }
+         if(flag)
+         {
+            if(document.getElementById("txt2").value==="true")
+            {
+                
+                _value = true;
+            }
+            else if(document.getElementById("txt2").value==="false")
+            {
+                
+                _value =false;
+
+            }
+            else if(parseInt(document.getElementById("txt2").value))
+            {
+                
+                _value =parseInt( document.getElementById("txt2").value);
+
+            }
+            else
+            {
+                
+                _value = document.getElementById("txt2").value;
+            }
            if(_key === "" || _value === "")
            {
                alert("Porfavor ingrese un valor");
@@ -18,9 +59,13 @@
                key: _key,
                 value: _value          
             });
-            alert(dictionary[0].value);
+           }           
+         }
+         else{
+             alert("La llave ingresada ya existe");
+         }
 
-           }
+         document.getElementById("txt1").value = "";
            
            
        }
@@ -28,17 +73,16 @@
 
        function ShowData()
        {
-           alert(JSON.stringify(dictionary[0]));
            document.getElementById("txtArea").value = JSON.stringify(dictionary);     
-           
-
        }
 
        function ConvertToXml()
        {
            var xml = "<Xml>\n <Dictionary>\n";
            for (var i = 0; i <dictionary.length; i++) {
-            xml += "   <Key>"+ dictionary[i].key+ "</Key><Value>"+ dictionary[i].value + "</Value>\n";
+            xml += "   <Element>\n";
+            xml += "     <Key>"+ dictionary[i].key+ "</Key>\n     <Value>"+ dictionary[i].value + "</Value>\n";
+            xml += "   </Element>\n";
          }
            xml += " </Dictionary>\n</Xml>";
            document.getElementById("txtArea").value = xml;  
